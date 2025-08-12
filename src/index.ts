db64f42da6caf8702fe208e55dc3dc8685f3416a
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-11 22:50:40 -0300

    Initialize Voltaris-CRM backend: set up Express server, configured PostgreSQL connection, created user and database roles, and fixed permission issues

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
-import { Request, Response } from "express";
+import cors from 'cors';
 import dotenv from "dotenv";
+import clientRoutes from "./routes/clientRoutes"
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Voltaris CRM Backend is running 🚀</h1>");
-});
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', clientRoutes)
+
 
 app.listen(port, () => {
   console.log("Server listening on port 3000");
---
+++ src/routes/clientRoutes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import * as clientController from '../controllers/clientController'
+
+const router = express.Router();
+
+router.get('/clients', clientController.getClients);
+router.post('/clients', clientController.createClient);
+
+export default router;
